5866477dd579d64eeeee7ce5a2b714d33b5eac92
Karl Matthias <karl@matthias.org>
2019-01-08 22:38:26 +0000

    Clean up warnings

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule EnvVarProvider.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      description: description,
-      package: package,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
