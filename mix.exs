7efb2dade0c6081cd2c4d92b969a4687f3be43b6
Tom Patterer <thomas@community.com>
2019-04-18 14:40:21 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EnvVarProvider.MixProject do
   def project do
     [
       app: :env_var_provider,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
