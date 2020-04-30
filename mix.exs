2f57181b04f8f9b830c2e1116cb7f7e1bfea01c0
Andrea Leopardi <an.leopardi@gmail.com>
2020-04-30 10:22:43 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EnvVarProvider.MixProject do
   def project do
     [
       app: :env_var_provider,
-      version: "0.6.0",
+      version: "0.5.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
